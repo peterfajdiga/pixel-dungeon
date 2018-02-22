5c01a52c54c341ff31ad5e421ccc145b8f5c2474
Peter Fajdiga <peter.fajdiga@gmail.com>
2018-02-22 16:11:06 +0100

    Customization.java reorder

--- src/com/watabou/pixeldungeon/Customization.java
+++ src/com/watabou/pixeldungeon/Customization.java
@@ -10,9 +10,12 @@ public final class Customization {
 
     public static final boolean ITEM_DAMAGE_ENABLED = false;
     public static final boolean UPGRADE_ALWAYS_UPGRADES = true;  // vanilla is false
+
     public static final boolean ALWAYS_NIGHT = false;  // takes precedence
     public static final boolean ALWAYS_DAY = true;
-    public static final int DISMANTLE_UPGRADE_CAP = Integer.MAX_VALUE;  // vanilla is 1
+
     public static final float LLOYDSBEACON_DROP_PROBABILITY = 0.333f;  // vanilla is 0.333f
     public static final boolean LLOYDSBEACON_SHOP = true;
+
+    public static final int DISMANTLE_UPGRADE_CAP = Integer.MAX_VALUE;  // vanilla is 1
 }
